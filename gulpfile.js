086b0399727e162d0df71abb1139e831887f35ac
Denis <denis.dmitriyeff@gmail.com>
2019-04-24 15:56:12 +0300

    implemented browserSync using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//complete scss into css
+function style() {
+  // 1. where is my scss file
+  return gulp.src('./mystyles/**/*.scss')
+  // 2. pass that file through sass compiler
+    .pipe(sass())
+  // 3. where do I save the compiled CSS?
+    .pipe(gulp.dest('./mystyles'))
+  // 4. stream change to all browser
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch('./mystyles/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;

df3f213f458c6165bf349f05b7f126c8cc3db87f
Denis <denis.dmitriyeff@gmail.com>
2019-04-25 11:30:23 +0300

    added items and displayed them

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -3,8 +3,26 @@
 angular
   .module('portfolioProject')
   .controller('ProjectCtrl', function () {
-
-    this.id = "memory-game";
-    this.name = "Memory Game";
-    this.description = "Build an interactive browser-based card matching game with combined skills in HTML, CSS, and JavaScript";
+    this.items = [
+      {
+        id: "memory-game",
+        name: "Memory Game",
+        description: "Built an interactive browser-based card matching game with combined skills in HTML, CSS, and JavaScript"
+      },
+      {
+        id: "arcade-game",
+        name: "Classic Arcade Game Clone",
+        description: "Leveraged object-oriented JavaScript to build a classic arcade game"
+      },
+      {
+        id: "feed-reader",
+        name: "Feed Reader Testing",
+        description: "Used Jasmine to develop tests against a pre-existing web application"
+      },
+      {
+        id: "restaurant-app",
+        name: "Restaurant Reviews App",
+        description: "Converted a static webpage to be responsive on different sized displays, and accessible for screen reader use"
+      }
+    ];
   });

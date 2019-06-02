31e7f4acefff55de8f296cade36c47bad5917f0d
Denis <denis.dmitriyeff@gmail.com>
2019-06-02 13:01:51 +0300

    added new project. changed the limit of projects per page.

--- scripts/controllers/projects.js
+++ scripts/controllers/projects.js
@@ -4,7 +4,7 @@ angular
   .module('portfolioProject')
   .controller('ProjectCtrl', ['projectsInfo', function(info) {
     var vm = this;
-    vm.pageSize = 4;
+    vm.pageSize = 6;
     vm.currentPage = 1;
 
     info.getProjects().then(function(response) {

ba9bd79f746116e9f8601d01b0c008df7ac13b84
Denis <denis.dmitriyeff@gmail.com>
2019-06-03 22:57:07 +0300

    version for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,55 @@
-
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
 const browserSync = require('browser-sync').create();
 
-//complete scss into css
-function style() {
-  // 1. where is my scss file
-  return gulp.src('./mystyles/**/*.scss')
-  // 2. pass that file through sass compiler
+var paths = {
+  styles: {
+    src: './mystyles/**/*.scss',
+    dest: 'assets/styles/'
+  },
+  scripts: {
+    src: './scripts/**/*.js',
+    dest: 'assets/scripts/'
+  }
+};
+
+/* Not all tasks need to use streams, a gulpfile is just another node program
+ * and you can use all packages available on npm, but it must return either a
+ * Promise, a Stream or take a callback and call it
+ */
+function clean() {
+  // You can use multiple globbing patterns as you would with `gulp.src`,
+  // for example if you are using del 2.0 or above, return its promise
+  return del([ 'assets' ]);
+}
+
+/*
+ * Define our tasks using plain functions
+ */
+function styles() {
+  return gulp.src(paths.styles.src)
     .pipe(sass())
-  // 3. where do I save the compiled CSS?
-    .pipe(gulp.dest('./mystyles'))
-  // 4. stream change to all browsers
-    .pipe(browserSync.stream());
+    .pipe(cleanCSS())
+    // pass in options to the stream
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
 function watch() {
@@ -22,10 +59,25 @@ function watch() {
       baseDir: './'
     }
   });
-  gulp.watch('./mystyles/**/*.scss', style);
+  gulp.watch(paths.styles.src, styles);
   gulp.watch('./**/*.html').on('change', browserSync.reload);
-  gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
+  gulp.watch(paths.scripts.src, scripts);
 }
 
-exports.style = style;
+/*
+ * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
+ */
+var build = gulp.series(clean, gulp.parallel(styles, scripts));
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
 exports.watch = watch;
+exports.build = build;
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;

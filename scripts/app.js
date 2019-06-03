632278c95d5018b6da798f943d76380625741799
Denis <denis.dmitriyeff@gmail.com>
2019-06-04 00:32:29 +0300

    installed angular-route

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-  .module('portfolioProject', ['ui.bootstrap'])
+  .module('portfolioProject', ['ui.bootstrap', 'ngRoute'])
   .filter('startFrom', function() {
     return function(data, start) {
       return data.slice(start);

3038b242082598dadbff7b04cb0b2fe05e714d8c
Denis <denis.dmitriyeff@gmail.com>
2019-06-04 10:54:03 +0300

    changed font size

--- assets/scripts/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular
-  .module('portfolioProject', ['ui.bootstrap'])
-  .filter('startFrom', function() {
-    return function(data, start) {
-      return data.slice(start);
-    }
-  });
